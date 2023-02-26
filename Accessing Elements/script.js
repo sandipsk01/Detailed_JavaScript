a9c3dfade944209817bf56f4f68ee60105d0bca6
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2023-02-26 15:52:15 +0530

    Delete Accessing Elements directory

--- Accessing Elements/script.js
+++
@@ -1,7 +0,0 @@
-var byId=document.getElementById('demo');
-byId.style.borderColor='green';
-
-var byCssSelector=document.querySelectorAll('.demo');
-byCssSelector[1].style.borderColor='blue';
-byCssSelector[0].style.borderColor='blue';
-byCssSelector[1].innerText="Second Elements of Class";
