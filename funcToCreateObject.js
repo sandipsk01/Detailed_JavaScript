cd92ce06353ccc7575fd822dff406930654372b0
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2023-03-02 12:14:30 +0530

    Update funcToCreateObject.js

--- funcToCreateObject.js
+++ funcToCreateObject.js
@@ -16,9 +16,12 @@ student2;
 function CreateStudent1(name, rollNo, marks){
     this.name=name;
     this.rollNo=rollNo;
-    this.marks=marks;
+    this.getRollNo=function(){
+        return this.rollNo;
+    };
 }
 
 var student01= new CreateStudent1("Sandip",1,30);   //called as constructor
 var student02= CreateStudent1("XYZ",2,20);      //callled as general function it will take value if we returning anything
 student01;
+// getRollNo:f will get created for every object. It will take space for every occurence i.e. waste of memory.
