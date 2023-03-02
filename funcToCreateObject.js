c8762e9a1b4dff84e75a7e7a7f6e76eaa79efd97
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2023-03-02 11:29:53 +0530

    Update funcToCreateObject.js

--- funcToCreateObject.js
+++ funcToCreateObject.js
@@ -10,3 +10,15 @@ var student1=createStudent("Sandip",1,30);
 var student2=createStudent("XYZ",2,20);
 student1;
 student2;
+
+// OR
+//Using Constructor
+function CreateStudent1(name, rollNo, marks){
+    this.name=name;
+    this.rollNo=rollNo;
+    this.marks=marks;
+}
+
+var student01= new CreateStudent1("Sandip",1,30);   //called as constructor
+var student02= CreateStudent1("XYZ",2,20);      //callled as general function it will take value if we returning anything
+student01;
